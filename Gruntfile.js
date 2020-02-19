8b6f97a1456f87712e385830203923c9a66de8f2
Manisha Dayal <manisha.dayal@vidyamantra.com>
2020-02-19 22:50:53 +0530

    Grunt changed

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,8 +23,16 @@
 /**
  * Grunt configuration
  */
-
+"use strict";
 module.exports = function(grunt) {
+	// We need to include the core Moodle grunt file too, otherwise we can't run tasks like "amd".
+    require("grunt-load-gruntfile")(grunt);
+    grunt.loadGruntfile("../../Gruntfile.js");
+ 
+    // Load all grunt tasks.
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
@@ -35,8 +43,29 @@ module.exports = function(grunt) {
                 src: 'amd/src/congrea.js',
                 dest: 'amd/build/congrea.min.js'
             }
+        },
+		watch: {
+            // If any .less file changes in directory "less" then run the "less" task.
+            files: "less/*.less",
+            tasks: ["less"]
+        },
+        less: {
+            // Production config is also available.
+            development: {
+                options: {
+                    // Specifies directories to scan for @import directives when parsing.
+                    // Default value is the directory of the source, which is probably what you want.
+                    paths: ["less/"],
+                    compress: true
+                },
+                files: {
+                    "styles.css": "less/styles.less"
+                }
+            },
         }
     });
+	    // The default task (running "grunt" in console).
+    grunt.registerTask("default", ["less"]);
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     // Default task(s).
